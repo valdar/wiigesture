f8adf778150eae6d43f374b130ba4be24e7124d4
Andrea Tarocchi <andrea.tarocchi@gmail.com>
2008-11-13 15:18:02 +0000

    Aggiornato wiiuse a versione 0.12.

--- wiiuse/main.cpp
+++ wiiuse/main.cpp
@@ -1,31 +1,63 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
+/*
+ *	wiiuse
+ *
+ *	Written By:
+ *		Michael Laforest	< para >
+ *		Email: < thepara (--AT--) g m a i l [--DOT--] com >
+ *
+ *	Copyright 2006-2007
+ *
+ *	This file is part of wiiuse.
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *	$Header$
+ *
+ */
+
+/**
+ *	@file
+ *
+ *	@brief Example using the wiiuse API.
+ *
+ *	This file is an example of how to use the wiiuse library.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "wiiuse.h"
 
-#ifdef __cplusplus
-}
-#endif
-
-#define WIIUSE_PATH	"./lib/wiiuse.so"
-#include <iostream>
 #include <fstream>
 
-std::ofstream outfile;
+#ifndef WIN32
+	#include <unistd.h>
+#endif
+
+#include "wiiuse.h"
+
+
+#define MAX_WIIMOTES    1
+
 
 /**
  *	@brief Callback that handles an event.
  *
- *	@param wm Pointer to a wiimote_t structure.
+ *	@param wm		Pointer to a wiimote_t structure.
  *
  *	This function is called automatically by the wiiuse library when an
  *	event occurs on the specified wiimote.
  */
-void handle_event(struct wiimote_t* wm) {
-
+void handle_event(struct wiimote_t* wm, std::ofstream* file) {
 
     if (wm->btns) {
 		/*
@@ -51,7 +83,7 @@ void handle_event(struct wiimote_t* wm) {
 		if(wm->btns){
 		    if(IS_JUST_PRESSED(wm, WIIMOTE_BUTTON_A)){
                 printf("INIZIO GESTURE\n");
-                outfile << "<gesture>" << std::endl;
+                *file << "<gesture>" << std::endl;
 		    }
 		}
 
@@ -61,18 +93,51 @@ void handle_event(struct wiimote_t* wm) {
                 printf("wiimote Y acc   = %f\n", wm->gforce.y);
                 printf("wiimote Z acc   = %f\n", wm->gforce.z);
 
-                outfile << wm->gforce.x << " " << wm->gforce.y << " " << wm->gforce.z << std::endl;
+                *file << wm->gforce.x << " " << wm->gforce.y << " " << wm->gforce.z << std::endl;
 		    }
         }
 
         if(wm->btns_released){
             if(IS_RELEASED(wm, WIIMOTE_BUTTON_A)){
                 printf("FINE GESTURE\n");
-                outfile << "</gesture>" << std::endl;
+                *file << "</gesture>" << std::endl;
 		    }
 		}
 	}
 }
+
+
+/**
+ *	@brief Callback that handles a read event.
+ *
+ *	@param wm		Pointer to a wiimote_t structure.
+ *	@param data		Pointer to the filled data block.
+ *	@param len		Length in bytes of the data block.
+ *
+ *	This function is called automatically by the wiiuse library when
+ *	the wiimote has returned the full data requested by a previous
+ *	call to wiiuse_read_data().
+ *
+ *	You can read data on the wiimote, such as Mii data, if
+ *	you know the offset address and the length.
+ *
+ *	The \a data pointer was specified on the call to wiiuse_read_data().
+ *	At the time of this function being called, it is not safe to deallocate
+ *	this buffer.
+ */
+void handle_read(struct wiimote_t* wm, byte* data, unsigned short len) {
+	int i = 0;
+
+	printf("\n\n--- DATA READ [wiimote id %i] ---\n", wm->unid);
+	printf("finished read of size %i\n", len);
+	for (; i < len; ++i) {
+		if (!(i%16))
+			printf("\n");
+		printf("%x ", data[i]);
+	}
+	printf("\n\n");
+}
+
 
 /**
  *	@brief Callback that handles a controller status event.
@@ -91,16 +156,16 @@ void handle_event(struct wiimote_t* wm) {
  *	One reason the status can change is if the nunchuk was
  *	inserted or removed from the expansion port.
  */
-void handle_ctrl_status(struct wiimote_t* wm, int attachment, int speaker, int ir, int led[4], float battery_level) {
+void handle_ctrl_status(struct wiimote_t* wm) {
 	printf("\n\n--- CONTROLLER STATUS [wiimote id %i] ---\n", wm->unid);
 
-	printf("attachment:      %i\n", attachment);
-	printf("speaker:         %i\n", speaker);
-	printf("ir:              %i\n", ir);
-	printf("leds:            %i %i %i %i\n", led[0], led[1], led[2], led[3]);
-	printf("battery:         %f %%\n", battery_level);
-
+	printf("attachment:      %i\n", wm->exp.type);
+	printf("speaker:         %i\n", WIIUSE_USING_SPEAKER(wm));
+	printf("ir:              %i\n", WIIUSE_USING_IR(wm));
+	printf("leds:            %i %i %i %i\n", WIIUSE_IS_LED_SET(wm, 1), WIIUSE_IS_LED_SET(wm, 2), WIIUSE_IS_LED_SET(wm, 3), WIIUSE_IS_LED_SET(wm, 4));
+	printf("battery:         %f %%\n", wm->battery_level);
 }
+
 
 /**
  *	@brief Callback that handles a disconnection event.
@@ -111,102 +176,57 @@ void handle_ctrl_status(struct wiimote_t* wm, int attachment, int speaker, int i
  *	if the connection is interrupted.
  */
 void handle_disconnect(wiimote* wm) {
-    outfile.close();
 	printf("\n\n--- DISCONNECTED [wiimote id %i] ---\n", wm->unid);
 }
 
-/*
- *	These are some identifiers for wiimotes
- *
- *	See below in main() for what they are used for.
- */
-#define WIIMOTE_ID_1		1
+
+void test(struct wiimote_t* wm, byte* data, unsigned short len) {
+	printf("test: %i [%x %x %x %x]\n", len, data[0], data[1], data[2], data[3]);
+}
+
+
 
 /**
  *	@brief main()
  *
- *	Connect to up to one wiimotes and print any events
+ *	Connect to up to two wiimotes and print any events
  *	that occur on either device.
  */
 int main(int argc, char** argv) {
 
     if(argc < 2)
         return 1;
-
+    std::ofstream outfile;
     outfile.open(argv[1]);
 
-	/*
 
-	 *	Make a temp array of wiimote ids.
-
-	 *
-
-	 *	Here I only anticipate connecting up to
-
-	 *	two wiimotes.  Each wiimote connected
-
-	 *	will get one of these ids.
-
-	 */
-
-	int ids[] = { WIIMOTE_ID_1 };
-
-
-
-	const char* version;
 
 	wiimote** wiimotes;
-
 	int found, connected;
-
-
-	/*
-	 *	Load the wiiuse library
-	 *
-	 *	This needs to be done before anything else can happen
-	 *	wiiuse_startup() will return the version of the library loaded.
-	 */
-
-	version = wiiuse_startup(WIIUSE_PATH);
-
-	printf("Wiiuse Version = %s\n", version);
-	if (!version) {
-		fprintf(stderr, "Failed to load wiiuse library.\n");
-		return 0;
-	}
 
 	/*
 	 *	Initialize an array of wiimote objects.
 	 *
-	 *	The first parameter is the number of wiimotes
-	 *	I want to create.  I only have one wiimotes
-	 *	so I'm limiting the test to just 1.
-	 *
-	 *	Then I get it the array of ids and a couple
-	 *	callback functions to invoke when something
-	 *	happens on one of the wiimotes.
-	 *
-	 *	handle_event gets called when a generic event occurs (button press, motion sensing, etc)
-	 *	handle_ctrl_status gets called when a response to a status request arrives (battery power, etc)
-	 *	handle_disconnect gets called when the wiimote disconnect (holding power button)
+	 *	The parameter is the number of wiimotes I want to create.
 	 */
-	wiimotes =  wiiuse_init(1, ids, handle_event, handle_ctrl_status, handle_disconnect);
+	wiimotes =  wiiuse_init(MAX_WIIMOTES);
 
 	/*
 	 *	Find wiimote devices
 	 *
 	 *	Now we need to find some wiimotes.
 	 *	Give the function the wiimote array we created, and tell it there
-	 *	are 1 wiimotes we are interested in.
+	 *	are MAX_WIIMOTES wiimotes we are interested in.
 	 *
 	 *	Set the timeout to be 5 seconds.
 	 *
 	 *	This will return the number of actual wiimotes that are in discovery mode.
 	 */
-
-	found = wiiuse_find(wiimotes, 1, 5);
-	if (!found)
+	found = wiiuse_find(wiimotes, MAX_WIIMOTES, 5);
+	if (!found) {
+		printf ("No wiimotes found.");
 		return 0;
+	}
 
 	/*
 	 *	Connect to the wiimotes
@@ -217,7 +237,7 @@ int main(int argc, char** argv) {
 	 *
 	 *	This will return the number of established connections to the found wiimotes.
 	 */
-	connected = wiiuse_connect(wiimotes, 1);
+	connected = wiiuse_connect(wiimotes, MAX_WIIMOTES);
 	if (connected)
 		printf("Connected to %i wiimotes (of %i found).\n", connected, found);
 	else {
@@ -230,11 +250,22 @@ int main(int argc, char** argv) {
 	 *	to tell which wiimotes are connected (just like the wii does).
 	 */
 	wiiuse_set_leds(wiimotes[0], WIIMOTE_LED_1);
+	//wiiuse_set_leds(wiimotes[1], WIIMOTE_LED_2);
+	//wiiuse_set_leds(wiimotes[2], WIIMOTE_LED_3);
+	//wiiuse_set_leds(wiimotes[3], WIIMOTE_LED_4);
 	wiiuse_rumble(wiimotes[0], 1);
-    usleep(200000);
-    wiiuse_rumble(wiimotes[0], 0);
+	//wiiuse_rumble(wiimotes[1], 1);
 
+	#ifndef WIN32
+		usleep(200000);
+	#else
+		Sleep(200);
+	#endif
 
+	wiiuse_rumble(wiimotes[0], 0);
+	//wiiuse_rumble(wiimotes[1], 0);
+
+    bool one_wiimote_connected = true;
 	/*
 	 *	Maybe I'm interested in the battery power of the 0th
 	 *	wiimote.  This should be WIIMOTE_ID_1 but to be sure
@@ -245,7 +276,7 @@ int main(int argc, char** argv) {
 	 *	if any expansions are plugged into the wiimote or
 	 *	what LEDs are lit.
 	 */
-	wiiuse_status(wiimotes[0]);
+	//wiiuse_status(wiimotes[0]);
 
 	/*
 	 *	This is the main loop
@@ -255,24 +286,86 @@ int main(int argc, char** argv) {
 	 *	(it doesn't matter if some of those wiimotes are not used
 	 *	or are not connected).
 	 *
-	 *	This function will invoke the callbacks set in wiiuse_init()
+	 *	This function will set the event flag for each wiimote
 	 *	when the wiimote has things to report.
 	 */
-	while (1) {
-		wiiuse_poll(wiimotes, 1);
+	while (1 && one_wiimote_connected) {
+		if (wiiuse_poll(wiimotes, MAX_WIIMOTES)) {
+			/*
+			 *	This happens if something happened on any wiimote.
+			 *	So go through each one and check if anything happened.
+			 */
+			int i = 0;
+			for (; i < MAX_WIIMOTES; ++i) {
+				switch (wiimotes[i]->event) {
+					case WIIUSE_EVENT:
+						/* a generic event occured */
+						handle_event(wiimotes[i], &outfile);
+						break;
+
+					case WIIUSE_STATUS:
+						/* a status event occured */
+						handle_ctrl_status(wiimotes[i]);
+						break;
+
+					case WIIUSE_DISCONNECT:
+					case WIIUSE_UNEXPECTED_DISCONNECT:
+						/* the wiimote disconnected */
+						handle_disconnect(wiimotes[i]);
+						one_wiimote_connected = false;
+						break;
+
+					case WIIUSE_READ_DATA:
+						/*
+						 *	Data we requested to read was returned.
+						 *	Take a look at wiimotes[i]->read_req
+						 *	for the data.
+						 */
+						break;
+
+					case WIIUSE_NUNCHUK_INSERTED:
+						/*
+						 *	a nunchuk was inserted
+						 *	This is a good place to set any nunchuk specific
+						 *	threshold values.  By default they are the same
+						 *	as the wiimote.
+						 */
+						 //wiiuse_set_nunchuk_orient_threshold((struct nunchuk_t*)&wiimotes[i]->exp.nunchuk, 90.0f);
+						 //wiiuse_set_nunchuk_accel_threshold((struct nunchuk_t*)&wiimotes[i]->exp.nunchuk, 100);
+						printf("Nunchuk inserted.\n");
+						break;
+
+					case WIIUSE_CLASSIC_CTRL_INSERTED:
+						printf("Classic controller inserted.\n");
+						break;
+
+					case WIIUSE_GUITAR_HERO_3_CTRL_INSERTED:
+						/* some expansion was inserted */
+						handle_ctrl_status(wiimotes[i]);
+						printf("Guitar Hero 3 controller inserted.\n");
+						break;
+
+					case WIIUSE_NUNCHUK_REMOVED:
+					case WIIUSE_CLASSIC_CTRL_REMOVED:
+					case WIIUSE_GUITAR_HERO_3_CTRL_REMOVED:
+						/* some expansion was removed */
+						handle_ctrl_status(wiimotes[i]);
+						printf("An expansion was removed.\n");
+						break;
+
+					default:
+						break;
+				}
+			}
+		}
 	}
 
 	/*
 	 *	Disconnect the wiimotes
 	 */
-	wiiuse_disconnect(wiimotes[0]);
+	wiiuse_cleanup(wiimotes, MAX_WIIMOTES);
 
-	/*
-	 *	Unload the wiiuse library
-	 */
-	wiiuse_shutdown();
-
-	printf("saluti a tutti!");
-
+    outfile.close();
 	return 0;
 }
+
